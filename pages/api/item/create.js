352c4e3411a9ccaba78ebe0944f4955706f9bd2d
Atsushi ITANI <darazu@uranus.dti.ne.jp>
2023-12-11 17:11:52 +0900

    * add create item for mongoDB.

--- pages/api/item/create.js
+++ pages/api/item/create.js
@@ -1,10 +1,16 @@
 // create.js
 
 import connectDB from '@/utils/database';
+import { ItemModel } from '@/utils/schemaModels';
 
-const createItem = (req, res) => {
-  connectDB();
-  return res.status(200).json({ message: req.body.title });
+const createItem = async (req, res) => {
+  try {
+    await connectDB();
+    await ItemModel.create(req.body);
+    return res.status(200).json({ message: 'アイテム作成成功' });
+  } catch (err) {
+    return res.tatus(400).json({ message: 'アイテム作成失敗' });
+  }
 };
 
 export default createItem;
