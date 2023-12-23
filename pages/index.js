9a9239791be289c9ea02cb646ba96f801c4b023a
Atsushi ITANI <darazu@uranus.dti.ne.jp>
2023-12-23 09:45:06 +0900

    * fix replacing getServerSideProps by useEffect.

--- pages/index.js
+++ pages/index.js
@@ -2,44 +2,47 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import Head from 'next/head';
-import { getUrl } from 'nextjs-current-url/server';
+import { useEffect, useState } from 'react';
 
 const ReadAllItems = (props) => {
+  const [itemData, setItemData] = useState('');
+
+  useEffect(() => {
+    const getAllData = async () => {
+      const origin = window.location.origin;
+      const resp = await fetch(`${origin}/api/item/readall`);
+      const allItems = await resp.json();
+      setItemData(allItems);
+    };
+    getAllData();
+  }, [ReadAllItems]);
+
   return (
     <div>
       <Head>
         <title>Next Market</title>
       </Head>
       <div className="grid-container-in">
-        {props.allItems.map((item) => (
-          <Link href={`/item/${item._id}`} key={item._id} className="card">
-            <Image
-              src={item.image}
-              width={750}
-              height={500}
-              priority={true}
-              alt="item-image"
-            />
-            <div className="texts-area">
-              <h2>￥{Number(item.price).toLocaleString()}</h2>
-              <h3>{item.title}</h3>
-              <p>{item.description.substring(0, 80)}...</p>
-            </div>
-          </Link>
-        ))}
+        {itemData &&
+          itemData.allItems.map((item) => (
+            <Link href={`/item/${item._id}`} key={item._id} className="card">
+              <Image
+                src={item.image}
+                width={750}
+                height={500}
+                priority={true}
+                alt="item-image"
+              />
+              <div className="texts-area">
+                <h2>￥{Number(item.price).toLocaleString()}</h2>
+                <h3>{item.title}</h3>
+                <p>{item.description.substring(0, 80)}...</p>
+              </div>
+            </Link>
+          ))}
       </div>
     </div>
   );
 };
 
 export default ReadAllItems;
-
-export const getServerSideProps = async (ctx) => {
-  const url = await getUrl({ req: ctx.req });
-  console.log(url);
-  const resp = await fetch(`${url.origin}/api/item/readall`);
-  const allItems = await resp.json();
-  return {
-    props: allItems,
-  };
-};
